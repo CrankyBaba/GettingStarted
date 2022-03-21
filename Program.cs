e985c03605daf6a04e37addc98fbeeb69b1afa9a
Atanu Mondal <mondalatanu1998@gmail.com>
2022-03-21 17:48:29 +0530

    initilizing variables.

--- Program.cs
+++ Program.cs
@@ -1,15 +1,35 @@
 ﻿class Program { 
     static void Main(string[] args)
     {
-        var number = 4;
-        var greeting = "Hello!";
-        System.Console.WriteLine(number);
-        System.Console.WriteLine(greeting);
-        System.Console.WriteLine(number+2);
-        number = 10;
-        System.Console.WriteLine(12 - number);
+        //Ctrl + k + c to comment 
+        //var number = 4;
+        //var greeting = "Hello!";
+        //System.Console.WriteLine(number);
+        //System.Console.WriteLine(greeting);
+        //System.Console.WriteLine(number+2);
+        //number = 10;
+        //System.Console.WriteLine(12 - number);
 
+        //learnign about implesit type error if no value assigned to bariable
 
+        //var tester;
+        //to check the type name we use getType()
+        //System.Console.WriteLine(1.GetType());
+
+        //to avoid implesit type error we can do the following
+        //System.Int32 number;
+        //System.String name;
+        //number = 10;
+        //name = "Atanu";
+        //System.Console.WriteLine(number);
+        //System.Console.WriteLine(name); 
+
+        //OR we can initilize variable like below as well
+        //System.String name2 = "Raj";
+        //System.Console.WriteLine(name2);
+        // shortcut of System.Int32 and String System.String is as belwo
+        int a = 10;
+        string b = "alpha";
     }
 
 }
