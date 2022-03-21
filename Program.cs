ad4a0866a892dfe947e47b14f264c2e37a67b674
Atanu Mondal <mondalatanu1998@gmail.com>
2022-03-21 16:13:29 +0530

    Getting started with variables and error.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program { 
+    static void Main(string[] args)
+    {
+        var number = 4;
+        var greeting = "Hello!";
+        System.Console.WriteLine(number);
+        System.Console.WriteLine(greeting);
+        System.Console.WriteLine(number+2);
+        number = 10;
+        System.Console.WriteLine(12 - number);
+
+
+    }
+
+}
